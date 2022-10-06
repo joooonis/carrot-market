1dcb921c76550d811fb3a640ac997b5afacf40c4
jooooonis <xowns3213@naver.com>
2022-10-07 01:02:57 +0900

    design : build ui component  window + ; -> emogi 입력 min-h-screen -> min-height : 100vh aspect-square w-10 -> div를 정사각형으로 만들어 줍니다.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-32 px-5 grid gap-5 font-sans ">
+    <div className="bg-slate-400 py-32 px-5 grid gap-5 font-sans min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-2xl max-w-xs">
         <div className="font-semibold text-2xl mb-2">Select Item</div>
         <div className="flex justify-between my-1">
@@ -43,7 +43,42 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-2xl max-w-xs">
+        <div className="flex justify-between items-center">
+          <span className="text-4xl relative -left-4">⬅️</span>
+          <div className="flex items-center">
+            <span className="text-lg font-semibold">⭐ 4.9</span>
+            <span className="text-xl p-2 shadow-lg rounded-lg ml-6">💗</span>
+          </div>
+        </div>
+        <div className="bg-slate-500 h-72 mt-4" />
+        <div className="flex flex-col mt-4">
+          <div className="text-xl font-semibold">Swoon Lounge</div>
+          <div className="text-sm text-gray-500">Chair</div>
+        </div>
+        <div className="flex justify-between items-center mt-2 mb-5">
+          <div className="">
+            <input type="radio" />
+            <input type="radio" />
+            <input type="radio" />
+          </div>
+          <div className="flex space-x-3 justify-between items-center">
+            <div className="flex justify-center items-center bg-slate-200 aspect-square w-8 rounded-lg">
+              -
+            </div>
+            <div className="font-semibold">1</div>
+            <div className="flex justify-center items-center bg-slate-200 aspect-square w-8 rounded-lg">
+              +
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <span className="text-3xl font-semibold">$450</span>
+          <button className="bg-blue-400 text-lg text-white rounded-lg px-12 py-3 border-none">
+            Add to cart
+          </button>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
     </div>
   );
