61dcb95fda659443861a31dd55225202048b9701
jooooonis <xowns3213@naver.com>
2022-10-08 16:35:23 +0900

    design : peer modifier peer를 지정하고 그 아래 형제요소에서 peer modifier를 지정할 수 있습니다.

--- pages/index.tsx
+++ pages/index.tsx
@@ -126,7 +126,31 @@ const Home: NextPage = () => {
             disabled
             className="disabled:opacity-0"
           />
-          <input type="submit" value="Login" />
+          <input type="submit" value="Login" className="cursor-pointer" />
+        </form>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-2xl">
+        <form className="flex flex-col space-y-2 p-5 border-2 border-stone-600">
+          <input
+            type="text"
+            required
+            placeholder="Username"
+            className="border p-1 peer border-gray-400 rounded-md "
+          />
+          <span className="hidden peer-invalid:block peer-invalid:text-red-500">
+            This input is invalid
+          </span>
+          <span className="hidden peer-valid:block peer-valid:text-teal-500">
+            Awesome username
+          </span>
+          <span className="hidden peer-hover:block peer-hover:text-amber-500">
+            Hello
+          </span>
+          <input
+            type="submit"
+            value="Login"
+            className="bg-teal-500 text-white p-2 rounded-lg cursor-pointer"
+          />
         </form>
       </div>
     </div>
