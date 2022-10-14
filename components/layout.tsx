629480fe4d47b7662b539a778ba4c17f3dbbee14
jooooonis <xowns3213@naver.com>
2022-10-15 01:59:46 +0900

    chore : fixed and max-w fixed로 positioning을 한 경우 기본 위치에서 어긋남으로 부모의 width를 받지 못하여 따로 max-width값을 주어야 합니다.

--- components/layout.tsx
+++ components/layout.tsx
@@ -16,12 +16,12 @@ export default function Layout({
 }: LayoutProps) {
   return (
     <div>
-      <div className="bg-white w-full text-lg font-medium py-3 fixed text-gray-800 border-b top-0 justify-center flex items-center">
+      <div className="bg-white w-full max-w-xl text-lg font-medium py-3 fixed text-gray-800 border-b top-0 justify-center flex items-center">
         {title ? <span>{title}</span> : null}
       </div>
       <div className={cls('pt-16', hasTabBar ? 'pb-16' : '')}>{children}</div>
       {hasTabBar ? (
-        <nav className="bg-white text-gray-800 border-t fixed bottom-0 pb-10 pt-3 flex justify-between items-center"></nav>
+        <nav className="bg-white max-w-xl text-gray-800 border-t fixed bottom-0 pb-10 pt-3 flex justify-between items-center"></nav>
       ) : null}
     </div>
   );
