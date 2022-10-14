835fc106321cea9594daf67678a951770b91db44
jooooonis <xowns3213@naver.com>
2022-10-15 01:58:27 +0900

    feat : Layout component Layout 컴포넌트를 만들고 props를 통해 page별로 Layout을 설정합니다.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="w-full max-w-xl mx-auto">
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </div>
   );
 }
