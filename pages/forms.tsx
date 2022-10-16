65da301f8ab403ae7e872eebfebc00b203147b9c
jooooonis <xowns3213@naver.com>
2022-10-16 21:55:45 +0900

    feat : type of login form

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,32 +1,40 @@
-import { useForm } from 'react-hook-form';
+import { FieldErrors, useForm } from 'react-hook-form';
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email?: string;
+}
 
 export default function Form() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm<LoginForm>();
 
-  const onValid = () => {
-    console.log(register);
+  const onValid = (data: LoginForm) => {
+    console.log(data);
   };
-  const onInvalid = () => {
-    console.log('error!');
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
   };
 
   return (
     <form onSubmit={handleSubmit(onValid, onInvalid)} className="flex flex-col">
       <input
-        {...register('username', { required: true })}
+        {...register('username', {
+          required: 'Email is required',
+          minLength: {
+            message: 'The username should be longer than 5 chars',
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="username"
       />
       <input
-        type="email"
-        {...register('email', { required: true })}
-        placeholder="email"
-      />
-      <input
         type="password"
-        {...register('password', { required: true })}
+        {...register('password', { required: 'Password is required' })}
         placeholder="password"
       />
+      <input type="email" {...register('email', {})} placeholder="email" />
       <input
         className="border border-gray-500 py-2 bg-black text-white"
         type="submit"
