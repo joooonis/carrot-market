bca5a7d505ce63ccfe0a407714d064e050e32104
jooooonis <xowns3213@naver.com>
2022-10-22 01:11:11 +0900

    chore : stop auth

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,13 +4,13 @@ import withHandler from '@libs/server/withHandler';
 import twilio from 'twilio';
 import smtpTransport from '@libs/server/email';
 
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const user = phone ? { phone: +phone } : { email };
+  if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + '';
-
-  const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
-
   const token = await client.token.create({
     data: {
       payload,
@@ -26,36 +26,38 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   });
 
   if (phone) {
-    const message = await twilioClient.messages.create({
-      messagingServiceSid: process.env.TWILIO_MSID,
-      to: process.env.MYPHONE!,
-      body: `Your login token is ${payload}.`,
-    });
-    console.log(message);
+    // const message = await twilioClient.messages.create({
+    //   messagingServiceSid: process.env.TWILIO_MSID,
+    //   to: process.env.MYPHONE!,
+    //   body: `Your login token is ${payload}.`,
+    // });
+    // console.log(message);
   } else if (email) {
-    const mailOptions = {
-      from: process.env.MAIL_ID,
-      to: process.env.MAIL_ID,
-      subject: 'Nomad Carrot Authentication Email',
-      text: `Authentication Code : ${payload}`,
-    };
-    const result = await smtpTransport.sendMail(
-      mailOptions,
-      (error: any, responses: any) => {
-        if (error) {
-          console.log(error);
-          return null;
-        } else {
-          console.log(responses);
-          return null;
-        }
-      },
-    );
-    smtpTransport.close();
-    console.log(result);
+    // const mailOptions = {
+    //   from: process.env.MAIL_ID,
+    //   to: process.env.MAIL_ID,
+    //   subject: 'Nomad Carrot Authentication Email',
+    //   text: `Authentication Code : ${payload}`,
+    // };
+    // const result = await smtpTransport.sendMail(
+    //   mailOptions,
+    //   (error: any, responses: any) => {
+    //     if (error) {
+    //       console.log(error);
+    //       return null;
+    //     } else {
+    //       console.log(responses);
+    //       return null;
+    //     }
+    //   },
+    // );
+    // smtpTransport.close();
+    // console.log(result);
   }
 
-  res.status(200).end();
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler('POST', handler);
