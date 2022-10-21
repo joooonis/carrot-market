6ca201fd31a9f7be09e6b53e08ff5ddc62e685cf
jooooonis <xowns3213@naver.com>
2022-10-21 18:42:50 +0900

    feat : token logic token 생성시에 connectOrCreate를 사용하여 user가 없으면 생성하고 연결되도록 처리합니다. user를 따로 생성하지 않아도 됩니다.

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,16 +3,25 @@ import client from '@libs/server/client';
 import withHandler from '@libs/server/withHandler';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { email, phone } = req.body;
-  const payload = email ? { email } : { phone: +phone };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + '';
+
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: { name: 'Anonymous', ...user },
+        },
+      },
     },
-    create: { name: 'Anonymous', ...payload },
-    update: {},
   });
-  console.log(user);
+
+  console.log(token);
   /* if (email) {
     user = await client.user.findUnique({
       where: {
