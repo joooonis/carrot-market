7e5714f49f4611a715a39c928b44e8f9584c0c34
jooooonis <xowns3213@naver.com>
2022-10-31 02:15:21 +0900

    feat : all posts

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,4 +1,3 @@
-import { withIronSessionApiRoute } from 'iron-session/next';
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
 import client from '@libs/server/client';
@@ -12,20 +11,50 @@ async function handler(
     body: { question },
     session: { user },
   } = req;
-
-  const post = await client.post.create({
-    data: {
-      question,
-      user: {
-        connect: { id: user?.id },
+  if (req.method === 'POST') {
+    const post = await client.post.create({
+      data: {
+        question,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
       },
-    },
-  });
-
-  res.json({
-    ok: true,
-    post,
-  });
+    });
+    res.json({
+      ok: true,
+      post,
+    });
+  }
+  if (req.method === 'GET') {
+    const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            wondering: true,
+            answers: true,
+          },
+        },
+      },
+    });
+    res.json({
+      ok: true,
+      posts,
+    });
+  }
 }
 
-export default withApiSession(withHandler({ methods: ['POST'], handler }));
+export default withApiSession(
+  withHandler({
+    methods: ['GET', 'POST'],
+    handler,
+  }),
+);
