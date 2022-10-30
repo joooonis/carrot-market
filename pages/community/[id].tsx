0b7f1b4458e0baaf3c42dcf65a457d8c8b99d416
jooooonis <xowns3213@naver.com>
2022-10-31 00:57:18 +0900

    feat : answer

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -7,6 +7,8 @@ import { Answer, Post, User } from '@prisma/client';
 import Link from 'next/link';
 import useMutation from '@libs/client/useMutation';
 import { cls } from '@libs/client/utils';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
 
 interface AnswerWithUser extends Answer {
   user: User;
@@ -26,13 +28,28 @@ interface CommunityPostResponse {
   isWondering: boolean;
 }
 
+interface AnswerForm {
+  answer: string;
+}
+
+interface AnswerResponse {
+  ok: boolean;
+  response: Answer;
+}
+
 const CommunityPostDetail: NextPage = () => {
   const router = useRouter();
 
   const { data, mutate } = useSWR<CommunityPostResponse>(
     router.query.id ? `/api/posts/${router.query.id}` : null,
   );
-  const [wonder] = useMutation(`/api/posts/${router.query.id}/wonder`);
+  const [wonder, { loading }] = useMutation(
+    `/api/posts/${router.query.id}/wonder`,
+  );
+
+  const [sendAnswer, { data: answerData, loading: answerLoading }] =
+    useMutation<AnswerResponse>(`/api/posts/${router.query.id}/answers`);
+  const { register, handleSubmit, reset } = useForm<AnswerForm>();
 
   const onWonderClick = () => {
     if (!data) return;
@@ -52,8 +69,21 @@ const CommunityPostDetail: NextPage = () => {
       },
       false,
     );
-    wonder({});
+    if (!loading) {
+      wonder({});
+    }
   };
+
+  const onValid = (form: AnswerForm) => {
+    if (answerLoading) return;
+    sendAnswer(form);
+  };
+
+  useEffect(() => {
+    if (answerData && answerData.ok) {
+      reset();
+    }
+  }, [answerData, reset]);
 
   return (
     <Layout canGoBack>
@@ -138,17 +168,17 @@ const CommunityPostDetail: NextPage = () => {
             </div>
           </div>
         ))}
-
-        <div className="px-4">
+        <form onSubmit={handleSubmit(onValid)} className="px-4">
           <TextArea
+            register={register('answer', { required: true })}
             name="description"
             placeholder="Answer this question!"
             required
           />
           <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
-            Reply
+            {answerLoading ? 'Loading...' : 'Reply'}
           </button>
-        </div>
+        </form>
       </div>
     </Layout>
   );
