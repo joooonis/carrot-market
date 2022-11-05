f04ed5b9198a6535afb13b8b64ce2ed8c97f3692
jooooonis <xowns3213@naver.com>
2022-11-06 01:48:49 +0900

    feat : useUser hook 로그인하지 않았을 경우 로그인페이지로 replace되도록 전역에서 useUser hook을 사용합니다

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -4,10 +4,10 @@ import useSWR from 'swr';
 
 export default function useUser() {
   const router = useRouter();
-  const { data, error, mutate } = useSWR('/api/users/me');
+  const { data, error } = useSWR('/api/users/me');
 
   useEffect(() => {
-    if (data && !data.ok) {
+    if (data && !data.ok && router.pathname != '/enter') {
       // 만약 data = { ok:false } 응답을 받으면 로그인페이지로
       router.replace('/enter');
     }
