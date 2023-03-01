f2690e1170d0fcdec0bbca39185524abc689cb78
jooooonis <xowns3213@naver.com>
2023-03-02 00:25:57 +0900

    feat : check user is Bot

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,14 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-export function middleware(req: NextApiRequest, res: NextApiResponse) {
-  console.log('global middleware!');
+import {
+  NextFetchEvent,
+  NextRequest,
+  NextResponse,
+  userAgent,
+} from 'next/server';
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const ua = userAgent(req);
+  if (!ua.isBot) {
+    return new NextResponse("plz don't be a bot. Be human,", {
+      status: 403,
+    });
+  }
 }
