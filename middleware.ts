878d0114ae1e80f9b2ae20f4bac6516630f2d7a4
jooooonis <xowns3213@naver.com>
2023-03-02 01:04:04 +0900

    feat : responses and redirect middleware에서 현재 request에 cookie가 안 담겨있으면 /enter페이지로 redirect 시킵니다.

--- middleware.ts
+++ middleware.ts
@@ -6,9 +6,15 @@ import {
 } from 'next/server';
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   const ua = userAgent(req);
-  if (!ua.isBot) {
+  if (ua.isBot) {
     return new NextResponse("plz don't be a bot. Be human,", {
       status: 403,
     });
+  }
+
+  if (!req.url.includes('/api')) {
+    if (!req.url.includes('/enter') && !req.cookies.has('carrotsession')) {
+      return NextResponse.redirect(new URL('/enter', req.url));
+    }
   }
 }
